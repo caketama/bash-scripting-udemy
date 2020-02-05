2f62e75505237486a0b8a45232b88484a2dd568e
cake <jarrettw@protonmail.com>
2020-02-05 13:16:09 -0500

    restuctured folders

---
+++ section1/for-loops.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+for COLOR in red green blue
+do
+    echo "COLOR: $COLOR"
+done
+
+COLORS="red green blue"
+for COLOR in $COLORS
+do
+    echo "COLOR: $COLOR"
+done
--- section1/my-shell.sh
+++ section1/my-shell.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
-MY_SHELL="bash"
+MY_SHELL="csh"
 echo "I like the $MY_SHELL shell."
+
+if [ "$MY_SHELL" = "bash" ]
+then
+    echo "You seem to like the bash shell."
+elif [ "$MY_SHELL" = "csh" ]
+then
+    echo "You seem to like the csh shell."
+else
+    echo "You don't seem to like the bash or csh shells." 
+fi
 
 # encapsulate variable in curly braces to add text to value i.e, bash + ing
 echo "I am ${MY_SHELL}ing on my keyboard"
